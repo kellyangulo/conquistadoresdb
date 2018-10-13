0c2814ffbe246ab442f671c898a333a7a8966ae4
Cristina Beltrán <17170564@itculiacan.edu.mx>
2018-10-13 12:18:07 -0600

    Consultas no triviales

--- 3.ConsultasConquistadoresBD.sql
+++ 3.ConsultasConquistadoresBD.sql
@@ -1,12 +1,12 @@
 use ConquistadoresBD
 GO
 
---1. MOSTRAR EL PROMEDIO DE ESTATURA DE NIÑOS Y NIÑAS DE CADA UNIDAD
+--1. MOSTRAR LA ESTATURA PROMEDIO DE NIÑOS DE CADA UNIDAD
 select AVG(n.estatura) as [Estatura Promedio] from ninoUnidad u 
 inner join nino n on u.nino_id = n.nino_id
 group by u.unidad_id
 
---2. MOSTRAR EL PROMEDIO DE PESO DE NIÑOS Y NIÑAS DE CADA UNIDAD
+--2. MOSTRAR EL PESO PROMEDIO DE NIÑOS Y NIÑAS DE CADA UNIDAD
 select AVG(n.peso) as [Peso Promedio] from ninoUnidad u 
 inner join nino n on u.nino_id = n.nino_id
 group by u.unidad_id
@@ -17,9 +17,10 @@ inner join nino n on t.trabajador_id = n.padre_id
 inner join persona p on t.trabajador_id = p.id
 group by t.trabajador_id
 
---4. MOSTRAR CUANTAS ACTIVIADES TIENE CADA CLASE POR CLUB                         mal
-select COUNT(ca.actividad_id) as [Número de actividades] from clubClase cu
-inner join clase cl on cu.clase_id = cl.id
-inner join claseActividad ca on cu.clase_id = ca.clase_id
-group by cu.clase_id
+--4. MOSTRAR CUANTAS ACTIVIADES TIENE CADA CLASE                 
+select COUNT(ca.actividad_id) as [Número de actividades] from clase cl
+inner join claseActividad ca on cl.id = ca.clase_id
+group by ca.clase_id
+
+
 
