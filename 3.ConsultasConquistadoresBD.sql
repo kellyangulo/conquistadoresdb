88542e6fdc265f631090b784249743ec9bad4ce4
Cristina Beltrán <17170564@itculiacan.edu.mx>
2018-10-14 23:16:14 -0600

    AGREGACION DE MÁS CONSUTAS

--- 3.ConsultasConquistadoresBD.sql
+++ 3.ConsultasConquistadoresBD.sql
@@ -22,5 +22,48 @@ select COUNT(ca.actividad_id) as [Número de actividades] from clase cl
 inner join claseActividad ca on cl.id = ca.clase_id
 group by ca.clase_id
 
+--5. MOSTRAR EL NOMBRE DE LOS CAMPAMENTOS SE HAN HECHO EN "BARRANCHOS" 
+select c.colonia, COUNT(c.colonia) as Cantidad from campamento c
+where c.colonia like '%Barranchos%'
+group by c.colonia
+
+--6. MOSTRAR EL NOMBRE DE LOS NIÑOS QUE SEAN ALERGICOS AL POLEN
+select p.nombre +' '+ p.apellidos [Niños alergicos al polen]  from nino n
+inner join persona p on n.nino_id = p.id
+inner join alergiaNino a on a.nino_id = n.nino_id
+where a.alergia_id = 2
+
+
+--7.NOMBRE DE LAS UNIDADES QUE ASISTIERON AL CAMPAMENTO 
+select u.nombre from campamento c
+inner join campamentoUnidad cu on c.id = cu.campamento_id
+inner join unidad u on cu.unidad_id = u.id
+where c.nombre like '%La flor%'
+
+--CHECAR
+--8.CUANTOS TRABAJADORES HAY POR CADA CLUB
+select COUNT(c.id) from club c
+inner join HistorialTrabajador h on c.id  = h.club_id
+inner join trabajador t on h.trabajador_id = t.trabajador_id
+group by c.id
+except 
+(select t.trabajador_id from trabajador t where t.estatus like 0)
+
+--9.
+
+--10.
+
+
+
+
+-------------------------------- MAL 
+
+--. MOSTRAR EL NOMBRE DE LAS UNIDADES QUE SOLO TENGAN NIÑOS MAYORES DE 10 AÑOS
+select u.nombre from unidad u
+union 
+(select fecha_nacimiento, datediff(yy,getdate(),fecha_nacimiento) from nino)
+inner join ninoUnidad nu on u.id = nu.unidad_id
+inner join nino n on nu.nino_id = n.nino_id
+where datediff(yy,getdate(),n.fecha_nacimiento)
 
 
